852290e73a1e1443ce5d7547f964fb7bbc02f533
Vladyslav Avidzba <wasssd1@gmail.com>
2022-06-27 20:05:57 +0300

    profile-done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import user from '../database/user.json';
+import Profile from './Profile/Profile';
+import styles from './App.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.container}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
